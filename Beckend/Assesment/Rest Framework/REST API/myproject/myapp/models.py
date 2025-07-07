e54c5434a6b4aff746497ce5798d94f6fe2cebb3
Imtisal0507 <ansariimtisal@gmail.com>
2025-07-07 11:39:45 +0530

    New Changes

--- Beckend/Assesment/Rest Framework/REST API/myproject/myapp/models.py
+++ Beckend/Assesment/Rest Framework/REST API/myproject/myapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Comment(models.Model):
-    username = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    postId = models.IntegerField()
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField(max_length=500)
 
     def __str__(self):
-        return f'{self.username}: {self.content[:30]}'
+        return f"{self.name} ({self.email})"
